ba755fbf703b7677f1ddd5ba4face0bc13936f5c
chen weihong <whchen1080@gmail.com>
2013-11-07 10:40:51 +0800

    增加token过期后如何处理的示例

--- everydo/client.py
+++ everydo/client.py
@@ -17,7 +17,7 @@ class EverydoApiClient(OCEverydoApi):
                        token_url= self.api_host + '/@@access_token')
 
         self.access_token = None
-
+        self.sites = []
     def __repr__(self):
         return '<EverydoClient OAuth2>'
 
@@ -28,8 +28,8 @@ class EverydoApiClient(OCEverydoApi):
     def auth_with_code(self, code):
         self.access_token = self.client.auth_code.get_token(code, redirect_uri=self.redirect_uri, header_format="Oauth2 %s")
 
-    def auth_with_token(self, token):
-        self.access_token = AccessToken(self.client, token, header_format="Oauth2 %s")
+    def auth_with_token(self, token, refresh_token=''):
+        self.access_token = AccessToken(self.client, token=token, refresh_token=refresh_token, header_format="Oauth2 %s")
 
     def auth_with_password(self, username, password, **opt):
         self.access_token = self.client.password.get_token(username=username,
@@ -41,10 +41,11 @@ class EverydoApiClient(OCEverydoApi):
             for name, value in app.items():
                 value['name'] = name
                 sites.append(value)
+        return sites
 
     @property
     def token_code(self):
-        return self.access_token and self.access_token.token
+        return getattr(self.access_token, 'token', None)
 
     @property
     def refresh_token_code(self):
@@ -55,8 +56,10 @@ class EverydoApiClient(OCEverydoApi):
         self.access_token = access_token.refresh()
 
     def get_site(self, site_name):
-        sites = self.list_sites()
-        for site in sites:
+        if not self.sites:
+            self.sites = self.list_sites()
+
+        for site in self.sites:
             if site['name'] == site_name:
                 client = WOApiClient(self.key, self.secret, site['url'], self.redirect_uri)
                 client.auth_with_token(self.token_code)
@@ -105,5 +108,16 @@ if __name__ == '__main__':
     wo_api = edo_api.get_site('default')
 
     # 调用特定的API
-    file_info = wo_api.files.file_info(file_id=9284298392)
+    file_info = auto_check(wo_api, wo_api.files.file_info, file_id=9284298392)
+
+
+def auto_check(api_obj, api_func, **kwargs):
+    result = api_func(**kwargs)
+    # 调用出错自动刷新Token，并将Token保存到数据库
+    if 'api_error' in result:
+        api_obj.refresh_token(api_obj.refresh_token_code)
+        save(api_obj.token_code, api_obj.refresh_token_code)
+        result = api_func(**kwargs)
+
+    return result
 
