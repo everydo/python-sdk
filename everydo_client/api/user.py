239818ce0c0b1aec4afab0c24605ebd081d8eb7e
Chen Weihong <whchen1080@gmail.com>
2013-11-18 18:00:36 +0800

    rename

--- everydo_client/api/user.py
+++ everydo_client/api/user.py
@@ -3,8 +3,8 @@ from .base import BaseApi
 
 class UserApi(BaseApi):
    
-    def user_info(self, user_id):
-        return self._get('/api_get_user_info', user_id=user_id)
+    def principal_info(self, user_id):
+        return self._get('/api_get_principal_info', user_id=user_id)
 
     def has_user(self, username):
         return self._get('/api_has_user', username=username)
@@ -12,8 +12,8 @@ class UserApi(BaseApi):
     def list_user_groups(self, key):
         return self._get('/api_list_user_groups', key=key)
 
-    def list_users_info(self, users):
-        return self._get('/api_list_users_info', users=users)
+    def list_principal_info(self, users):
+        return self._get('/api_list_principal_info', users=users)
 
     def get_online_users_list(self, account):
         return self._get('/api_get_online_users_list', account=account)
